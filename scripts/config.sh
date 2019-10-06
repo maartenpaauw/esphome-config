af9e6a82000c67dfc549f00b85b3d74eb3178e5d
maartenpaauw <maartenpaauw@gmail.com>
2019-10-06 15:49:41 +0200

    chore: rename s20 devices

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,6 +3,6 @@
 esphome arilux_e27_1.yaml config;
 esphome arilux_e27_2.yaml config;
 esphome sonoff_basic_1.yaml config;
-esphome sonoff_s20_airco.yaml config;
-esphome sonoff_s20_wall_fan.yaml config;
+esphome sonoff_s20_1.yaml config;
+esphome sonoff_s20_2.yaml config;
 esphome sonoff_touch_dressing_room.yaml config;
